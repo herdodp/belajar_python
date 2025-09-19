e66e51b1ec1999f28f302508c206af6c76a2a0c9
herdo dimas pratirto <66925023+herdodp@users.noreply.github.com>
2025-09-19 15:48:02 +0700

    casting type data

---
+++ casting.py
@@ -0,0 +1,22 @@
+# casting tipe data pada variable angka ke tipe data yang lain
+
+angka = 0
+
+# integer to float
+angkaFloat = float(angka)
+print(angkaFloat)
+print(type(angkaFloat))
+
+# integer to string
+angkaString = str(angka)
+print(angkaString)
+print(type(angkaString))  
+
+# integer to boolean
+angkaBoolean = bool(angka)
+print(angkaBoolean)
+print(type(angkaBoolean))
+
+
+
+ 
